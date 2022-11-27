bb13daef96fce8c246b92c87400f5464d3d9159f
MichaelMironov <nemiron94@gmail.com>
2022-11-27 18:36:27 +0300

    move faker object to date generaotor

--- src/test/java/com/demoqa/tests/BaseTest.java
+++ src/test/java/com/demoqa/tests/BaseTest.java
@@ -2,14 +2,11 @@ package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import com.github.javafaker.Faker;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import java.util.Objects;
 
 public class BaseTest {
-
-    protected final Faker faker = new Faker();
 
     @BeforeAll
     static void setUp(){
