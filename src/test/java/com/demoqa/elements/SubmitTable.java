2e16421a65c9fb64b7a6e900ad589d2d16be7513
MichaelMironov <nemiron94@gmail.com>
2022-11-27 18:34:52 +0300

    overload expected value method

--- src/test/java/com/demoqa/elements/SubmitTable.java
+++ src/test/java/com/demoqa/elements/SubmitTable.java
@@ -4,21 +4,29 @@ import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 
+import java.util.Arrays;
+
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class SubmitTable {
 
     private final SelenideElement table = $(".table-responsive").find(By.tagName("tbody"));
-    private String row;
+    private SelenideElement row;
 
     public SubmitTable field(String name){
-       row = table.$(byText(name)).sibling(0).innerText();
+       row = table.$(byText(name));
        return this;
     }
 
     public SubmitTable expectedValue(String expectedValue){
-        Assertions.assertEquals(expectedValue, row, "Поля не равны");
+        Assertions.assertEquals(expectedValue, row.sibling(0).innerText(), "Поле: " + row.innerText());
+        return this;
+    }
+
+    public SubmitTable expectedValue(String[] expectedValue){
+        String expectedResult = Arrays.toString(expectedValue).replaceAll("^\\[|\\]$", "");
+        Assertions.assertEquals(expectedResult, row.sibling(0).innerText(), "Поле: " + row.innerText());
         return this;
     }
 }
